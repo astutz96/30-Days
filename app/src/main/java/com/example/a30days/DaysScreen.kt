de29daafbd46830f28a6d54441d6695215b3847e
astutz96 <astutz96.development@protonmail.com>
2023-09-04 00:01:29 -0400

    Adding days screen and some initial formatting.
    Populating column w/ days item data and

---
+++ app/src/main/java/com/example/a30days/DaysScreen.kt
@@ -0,0 +1,36 @@
+package com.example.a30days
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.a30days.model.Day
+
+@Composable
+fun DaysItem(day: Day, modifier: Modifier = Modifier) {
+
+    Card(elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)) {
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(text = stringResource(day.indicator))
+            Text(text = stringResource(day.tip))
+            Image(painter = painterResource(day.imgResourceId), contentDescription = null)
+            Text(text = stringResource(id = day.description))
+        }
+    }
+}
--- app/src/main/java/com/example/a30days/MainActivity.kt
+++ app/src/main/java/com/example/a30days/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.a30days
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a30days.data.Datasource
 import com.example.a30days.ui.theme._30DaysTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +35,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DaysApp()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DaysApp() {
+    val daysData = Datasource().loadDaysData()
+
+    Scaffold(topBar = { DaysTopAppBar() }) { it ->
+        LazyColumn(
+            contentPadding = it,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.padding(start = 12.dp, end = 12.dp)
+        ) {
+            items(daysData) {
+                DaysItem(day = it)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DaysTopAppBar(modifier: Modifier = Modifier){
+    TopAppBar(title = {
+        Text(
+            text = stringResource(R.string.app_name)
+        )
+    }, modifier = modifier)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     _30DaysTheme {
-        Greeting("Android")
+        DaysApp()
     }
 }
